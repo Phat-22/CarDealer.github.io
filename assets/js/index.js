a93eeefc41c524aa5b887c045ffdcb187da2fe0f
táhP <115966894+Phat-22@users.noreply.github.com>
2023-11-02 17:03:38 +0700

    Add files via upload

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,15 +13,15 @@ function loadProducts() {
             itemEl.classList.add("products--items");
             itemEl.innerHTML = `
                 <div class="products--items__img">
-                    <a href="./product-details?id=${item.id}"><img src="${item.image1}" alt="" /></a>
+                    <a href="./product-details.html?id=${item.id}"><img src="${item.image1}" alt="" /></a>
                     <div class="overlay">
                         <div class="text">
-                            <a href="./product-details?id=${item.id}"><img src="${item.image2}" alt="" /></a>
+                            <a href="./product-details.html?id=${item.id}"><img src="${item.image2}" alt="" /></a>
                         </div>
                     </div>
                 </div>
                 <div class="products--items__main">
-                    <a href="./product-details?id=${item.id}">${item.name}</a>
+                    <a href="./product-details.html?id=${item.id}">${item.name}</a>
                     <p>$${item.price}</p>
                     <button class="border__button">Thêm vào giỏ hàng</button>
                 </div>
--- assets/js/products-details.js
+++ assets/js/products-details.js
@@ -40,7 +40,7 @@ function detailsProducts() {
                                 </select>
                             </p>
                             <div class="quantity__addtocart">
-                                <input type="number" min="1" max="10" placeholder="Quantity" />
+                                <input class="quantity" type="number" min="1" max="10" placeholder="Quantity" />
                                 <button id="cart" class="border__button radius">Add to cart</button>
                             </div>
                             <p><strong>Category: </strong> ${target.category}</p>
@@ -72,9 +72,8 @@ function detailsProducts() {
                 `;
         const inputEl = detailsEl.querySelector('.quantity');
         let quantityInput;
-        inputEl.addEventListener('change', (e) => quantityInput = e.target.value);
+        inputEl.addEventListener('change', (event) => quantityInput = event.target.value);
         const buttonEl = detailsEl.querySelector('#cart');
-        console.log(buttonEl);
         buttonEl.addEventListener('click', () => {
             addToCart(target, quantityInput);
             cartNumber();
